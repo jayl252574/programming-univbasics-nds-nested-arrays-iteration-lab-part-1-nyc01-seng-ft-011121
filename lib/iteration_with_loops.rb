3b0f0a515c0c6514c819d52b3035fe283ebea40d
Jay Liu <jayl252574@gmail.com>
2020-12-26 17:21:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
-#array_arrays = [
-  #[10, 11],
-  #[99, 50, 3, 4],
-  #[23, 41]
-  #]
+array_arrays = [
+  [10, 11],
+  [99, 50, 3, 4],
+  [23, 41]
+  ]
     row_index = 0
   while row_index < src.count do
     element_index = 0
